40d11c462504975ea9df3088976e6f858df6fe5e
Bill Dami <bdami@gavant.com>
2023-10-10 14:27:07 -0400

    text change

--- initproject.js
+++ initproject.js
@@ -9,7 +9,7 @@ console.log(`Setting app name as ${APP_NAME}...`);
 cp.execSync(`LC_ALL=C find . -type f ! -name "initproject.js" -exec sed -i '' s/${APP_NAME_TOKEN}/${APP_NAME}/g {} +`);
 // TODO use template to replace README.md contents w/app-specific info
 // install deps
-console.log(`Installing dependencies, this may take some time...`);
+console.log(`Installing dependencies, this may take awhile...`);
 cp.execSync('yarn install');
 // initialize git repo
 console.log(`Initializing new git repo...`);
