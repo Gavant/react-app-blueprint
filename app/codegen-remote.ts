251d03888159d218244ba32c888fe9002362d97e
Austin Rich <arich@gavant.com>
2024-09-20 13:01:16 -0400

    Codegen updates

--- app/codegen-remote.ts
+++ app/codegen-remote.ts
@@ -1,18 +1,14 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-// TODO: Aspirational.
 const config: CodegenConfig = {
-    overwrite: true,
-    schema: 'https://{{APP_NAME}}.s3.amazonaws.com/schema.graphql',
-    documents: 'src/**/*.gql.ts',
     generates: {
-        'src/core/types/generated/': {
-            plugins: ['typescript', 'typescript-operations'],
-        },
         'src/core/types/generated/graphql.schema.json': {
+            documents: 'src/**/!(*.strapi).gql.ts',
             plugins: ['introspection'],
+            schema: process.env.SCHEMA_PATH || 'SOME-FALLBACK-PATH',
         },
     },
+    overwrite: true,
 };
 
 export default config;
--- app/codegen.ts
+++ app/codegen.ts
@@ -1,17 +1,14 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    overwrite: true,
-    schema: 'schema/schema.graphql',
-    documents: 'src/**/*.gql.ts',
     generates: {
-        'src/core/types/generated/graphql.ts': {
-            plugins: ['typescript', 'typescript-operations'],
-        },
         'src/core/types/generated/graphql.schema.json': {
+            documents: 'src/**/!(*.strapi).gql.ts',
             plugins: ['introspection'],
+            schema: 'schema/schema.graphql',
         },
     },
+    overwrite: true,
 };
 
 export default config;
