aec5572087c057884ee43b31f983bea3a9b7a53d
Adam Baker <bakerac4@gmail.com>
2024-10-04 09:44:57 -0400

    Cleanup of apollo auth management (#11)

--- app/src/core/hooks/useApolloError.tsx
+++ app/src/core/hooks/useApolloError.tsx
@@ -1,14 +1,16 @@
+import { useCallback } from 'react';
 import useSignOut from 'react-auth-kit/hooks/useSignOut';
-import Result, { err } from 'true-myth/result';
+import Result, { err, unwrapOr } from 'true-myth/result';
 
+import { StringErrorCodes } from '~/core/constants/errors';
 import useToast from '~/core/hooks/useToast';
 import {
-    TryGetApolloErrorResponseMessage,
     getStatusCode,
     handle400Error,
+    handle500Error,
     isApolloError,
-    options400 as options400Type,
-    options500 as options500Type,
+    OptionsFor400Error as options400Type,
+    OptionsFor500Error as options500Type,
 } from '~/core/utils/apolloError';
 
 export interface Options {
@@ -21,35 +23,35 @@ function useApolloError() {
     const { toast } = useToast();
     const signOut = useSignOut();
 
+    const showToastError = useCallback(
+        (message: string, callback: (() => void) | undefined) => {
+            toast.error(message);
+            setTimeout(() => {
+                callback?.();
+            });
+        },
+        [toast]
+    );
+
     return (error: any, { options400, reset }: Options = {}): Result<any, any> => {
-        let responseError: string | undefined = 'Unknown error occurred';
         if (isApolloError(error)) {
-            const statusCode = getStatusCode(error) ?? -1;
+            const statusCode = unwrapOr(-1, getStatusCode(error));
             if (statusCode > 399 && statusCode < 500) {
-                responseError = handle400Error(error, {
+                return handle400Error(error, {
                     ...options400,
                     redirectOn401: options400?.redirectOn401 ?? false,
                     signOut,
                 });
             } else {
-                const message = TryGetApolloErrorResponseMessage(error) ?? error.networkError?.message ?? 'Unknown error occurred';
-                toast.error(message);
-                setTimeout(() => {
-                    reset?.();
-                });
-                responseError = message;
+                const result = handle500Error(error);
+                const message = unwrapOr('Unknown error occurred', result) as string;
+                showToastError(message, reset);
+                return result;
             }
         } else {
-            toast.error('Unknown error occurred');
-            setTimeout(() => {
-                reset?.();
-            });
+            showToastError(StringErrorCodes.Unknown, reset);
+            return err(StringErrorCodes.Unknown);
         }
-
-        if (responseError) {
-            toast.error(responseError);
-        }
-        return err(responseError);
     };
 }
 
