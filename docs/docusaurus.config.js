934083ab3cbdc54b9260e886661443035eda2544
Austin Rich <arich@gavant.com>
2025-01-22 14:11:04 -0500

    Stray app_name debug

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
             // Replace with your project's social card
             image: 'img/docusaurus-social-card.jpg',
             navbar: {
-                title: '{{APP_NAME1}}',
+                title: '{{APP_NAME}}',
                 logo: {
                     alt: 'My Site Logo',
                     src: 'img/logo.svg',
