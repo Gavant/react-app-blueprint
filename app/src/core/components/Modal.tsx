6b8c3d10b18f178a9324c825d4d1fdd55174c4a0
Adam Baker <bakerac4@gmail.com>
2025-01-28 14:58:08 -0500

    Adding modal into blueprint with tests (#34)

---
+++ app/src/core/components/Modal.tsx
@@ -0,0 +1,60 @@
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { DialogActions, DialogTitle } from '@mui/material';
+import { ReactNode } from 'react';
+
+interface ResponsiveModalProps extends Omit<DialogProps, 'title'> {
+    /**
+     * Callback function to be called when the modal is closed
+     *
+     * @memberof ResponsiveModalProps
+     */
+    onCloseCallback?: () => void;
+    /**
+     * Disables the close button on the modal
+     *
+     * @memberof ResponsiveModalProps
+     */
+    closeDisabled?: boolean;
+    /**
+     * Action children to be rendered in the modal
+     *
+     * @memberof ResponsiveModalProps
+     */
+    actions?: ReactNode;
+    /**
+     * Overrides the default fullscreen behavior of showing full screen based upon the theme breakpoints (md or less)
+     *
+     * @memberof ResponsiveModalProps
+     */
+    fullScreen?: boolean;
+    /**
+     * Title of the modal
+     *
+     * @memberof ResponsiveModalProps
+     */
+    title?: ReactNode;
+}
+
+export default function Modal({ title, children, onCloseCallback, closeDisabled, actions, ...rest }: ResponsiveModalProps) {
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const onClose = () => {
+        if (!closeDisabled) {
+            if (onCloseCallback && typeof onCloseCallback === 'function') {
+                onCloseCallback();
+            }
+        }
+    };
+
+    return (
+        <Dialog fullScreen={fullScreen} {...rest} onClose={onClose}>
+            {title && <DialogTitle>{title}</DialogTitle>}
+            <DialogContent>{children}</DialogContent>
+            {actions && <DialogActions>{actions}</DialogActions>}
+        </Dialog>
+    );
+}
