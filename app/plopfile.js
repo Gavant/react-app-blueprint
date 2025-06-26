e48c64c930db451fb93dad6fe5acb4c8a3ca2e5a
Adam Baker <bakerac4@gmail.com>
2025-06-26 09:20:39 -0400

    Upgrading react-router (#71)

--- app/plopfile.js
+++ app/plopfile.js
@@ -1,65 +1,65 @@
 export default function (plop) {
     plop.setGenerator('component', {
+        actions: [
+            {
+                path: 'src/{{path}}/{{camelCase name}}.tsx',
+                templateFile: 'templates/component/component.js.hbs',
+                type: 'add',
+            },
+            {
+                path: 'src/{{path}}/__tests__/{{camelCase name}}.test.tsx',
+                skip: (data) => !data.testsFolder,
+                templateFile: 'templates/component/__tests__/component.test.js.hbs',
+                type: 'add',
+            },
+        ],
         description: 'Create a component',
         prompts: [
             {
-                type: 'input',
-                name: 'name',
                 message: 'What is the name of the component?',
-            },
-            {
+                name: 'name',
                 type: 'input',
-                name: 'path',
+            },
+            {
                 message: 'What path should this component live under? (src/[your answer here]/. Omit trailing slash.)',
+                name: 'path',
+                type: 'input',
             },
             {
-                type: 'confirm',
-                name: 'styledComponents',
                 message: 'Do you want to add a styled component wrapper?',
-            },
-            {
+                name: 'styledComponents',
                 type: 'confirm',
-                name: 'testsFolder',
+            },
+            {
                 message: 'Do you want to generate a tests folder?',
-            },
-        ],
-        actions: [
-            {
-                type: 'add',
-                path: 'src/{{path}}/{{camelCase name}}.tsx',
-                templateFile: 'templates/component/component.js.hbs',
-            },
-            {
-                type: 'add',
-                path: 'src/{{path}}/__tests__/{{camelCase name}}.test.tsx',
-                templateFile: 'templates/component/__tests__/component.test.js.hbs',
-                skip: (data) => !data.testsFolder,
+                name: 'testsFolder',
+                type: 'confirm',
             },
         ],
     });
     plop.setGenerator('feature', {
+        actions: [
+            {
+                path: 'src/features/{{camelCase name}}/public/{{pascalCase name}}View.tsx',
+                templateFile: 'templates/component/component.tsx.hbs',
+                type: 'add',
+            },
+            {
+                path: 'src/features/{{camelCase name}}/fence.json',
+                templateFile: 'templates/feature/fence.json.hbs',
+                type: 'add',
+            },
+            {
+                path: 'src/features/{{camelCase name}}/components',
+                type: 'add',
+            },
+        ],
         description: 'Create feature base',
         prompts: [
             {
-                type: 'input',
-                name: 'name',
                 message: 'What is the name of the feature?',
-            },
-        ],
-        actions: [
-            {
-                type: 'add',
-                path: 'src/features/{{camelCase name}}/public/{{pascalCase name}}View.tsx',
-                templateFile: 'templates/component/component.tsx.hbs',
-            },
-            {
-                type: 'add',
-                path: 'src/features/{{camelCase name}}/fence.json',
-                templateFile: 'templates/feature/fence.json.hbs',
-            },
-            {
-                type: 'add',
-                path: 'src/features/{{camelCase name}}/components',
+                name: 'name',
+                type: 'input',
             },
         ],
     });
